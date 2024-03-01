d751810344614fe83ecb8a9b835dfdf3e4ab8a1d
whugoigo <154397123+whugoigo@users.noreply.github.com>
2024-03-02 01:25:51 +0500

    Update User.java

--- src/main/java/kz/aitu/assik4sp/models/User.java
+++ src/main/java/kz/aitu/assik4sp/models/User.java
@@ -11,11 +11,12 @@ import java.sql.Date;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int userid;
-    private String fname;
-    private String sname;
-    private Date dateofbirth;
-    private int height;
-    private int weight;
-    private String gender;
+    private int userid; // Unique identifier for each user
+
+    private String fname; // First name of the user
+    private String sname; // Surname of the user
+    private Date dateofbirth; // Date of birth of the user
+    private int height; // Height of the user in centimeters
+    private int weight; // Weight of the user in kilograms
+    private String gender; // Gender of the user 
 }
