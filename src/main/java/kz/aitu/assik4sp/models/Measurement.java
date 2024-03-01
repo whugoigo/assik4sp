4a476e89406662ebf192caa478c613e720422b44
whugoigo <154397123+whugoigo@users.noreply.github.com>
2024-03-02 01:24:33 +0500

    added Measurement.java

--- src/main/java/kz/aitu/assik4sp/models/Measurement.java
+++ src/main/java/kz/aitu/assik4sp/models/Measurement.java
@@ -1,4 +1,5 @@
 package kz.aitu.assik4sp.models;
+
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -10,9 +11,10 @@ import java.sql.Date;
 public class Measurement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int measurementid;
-    private String type;
-    private Date date;
-    private double value;
-    private String unit;
+    private int measurementid; // Unique identifier for each measurement
+
+    private String type; // Type of measurement
+    private Date date; // Date when the measurement was taken
+    private double value; // Value of the measurement
+    private String unit; // Unit of measurement (e.g., cm, kg, etc.)
 }
